d1b9898ebea778c0e8fdf840ee5d790e62d697a7
UKnow-IKnow <sarojsahu014@gmail.com>
2022-08-17 12:22:39 +0530

    design todoForm and handle state

--- todo_app/src/components/TodoForm.js
+++ todo_app/src/components/TodoForm.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { Container, TextField, Button} from '@mui/material';
+import { useState } from 'react';
 
-const TodoForm = () => {
+const TodoForm = (onSubmit) => {
+
+
+  const [input, setInput] = useState("")
+
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // onSubmit({
+    //   id:Math.floor(Math.random() * 1000),
+    //   Text:input
+    // });
+
+     setInput('')
+  }
+
   return (
-    <div>TodoForm</div>
+    <Container
+       sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space=between",
+        marginTop: "1.5vh",
+        width: "38vw",
+       }}
+    >
+      <form
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          name='text'
+          id="outlined-basic"
+          type={"text"}
+          size='medium'
+          value={input}
+          onChange = {handleChange}
+          width = "30vw"
+          placeholder="Enter your Task"
+          variant="outlined"
+        />
+      </form>
+      <Button
+        sx={{
+          height: '5.5vh',
+          margin:'1vh'
+        }}
+        variant='contained'
+      >
+        Add todo
+      </Button>
+    </Container>
   )
 }
 
